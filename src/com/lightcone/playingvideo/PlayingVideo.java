b9f87ce76c37068f38b34f742210bc758a7c3310
kjijogeorge <jijogeorge277@gmail.com>
2013-01-25 20:08:12 +0530

    Initial commit
    Signed-off-by: kjijogeorge <jijogeorge277@gmail.com>

--- src/com/lightcone/playingvideo/PlayingVideo.java
+++ src/com/lightcone/playingvideo/PlayingVideo.java
@@ -9,6 +9,10 @@ import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaPlayer.OnPreparedListener;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.View;
@@ -16,22 +20,32 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.MediaController;
 import android.widget.MediaController.MediaPlayerControl;
+import android.widget.Toast;
 import android.view.SurfaceView;
 import android.os.Handler;
+import android.provider.Settings.SettingNotFoundException;
 
 public class PlayingVideo extends Activity implements OnCompletionListener, OnPreparedListener, SurfaceHolder.Callback, MediaPlayerControl {
 	
+	private static final double BATTERY_CONST = 313;
+	private static final double BRIGHT_CONST = 0.8892;
+	private static final double BITRATE_CONST = 0.0156;
+	private static final int ID_MENU_PRESENT = 1;
 	//static private final String pathToFile = "http://192.168.1.249:8800/ice.webm";  // Video source file
 	private static String TAG="Playing Video";
 	private SurfaceView surfaceView;
 	private SurfaceHolder surfaceHolder;
 	private static MediaPlayer videoPlayer;
 	private MediaController mController;
-	private Button buttonPlay;
-	public EditText editTextSongURL;
-	private Intent myIntent;
+	static Button buttonPlay;
+	static EditText editTextSongURL;
+	private MediaMetadataRetriever metaRetriever = new MediaMetadataRetriever();
+	double bRate;
+	int curBrightness;
+	double rTime;
+	double bLevel;
 	
-    /** Called when the activity is first created. */
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,28 +55,41 @@ public class PlayingVideo extends Activity implements OnCompletionListener, OnPr
         surfaceHolder = surfaceView.getHolder();
         surfaceHolder.addCallback(this);
         surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        //videoPlayer = (VideoView) findViewById(R.id.videoPlayer);   
-       // MediaController mediaController = new MediaController(this);
-        
+         
         editTextSongURL = (EditText)findViewById(R.id.EditTextSongURL);
-        editTextSongURL.setText("http://192.168.1.249:8800/ice.webm");
+        editTextSongURL.setText("http://192.168.1.249:8800/ice_med.webm");
 
         buttonPlay = (Button)findViewById(R.id.buttonGo);
         buttonPlay.setOnClickListener(new View.OnClickListener(){
-        	public void onClick(View v) {
+        	public void onClick(final View v) {
         		        		       		
         		if(videoPlayer!=null && videoPlayer.isPlaying()){
 	        	    	videoPlayer.stop();
 	        			Log.e(TAG, "Media Player is busy.Please try after sometime");
 	        			return;
         	    }	
-        		playVideo();
+        		
+        		Log.d(TAG, "File played :  " + editTextSongURL.getText().toString());
+        		playVideo(editTextSongURL.getText().toString());
+        		//Do something after 5000ms
+        	    final Handler handler = new Handler();
+        	    handler.postDelayed(new Runnable() {
+        	      @Override
+        	      public void run() {
+        	    	  if(videoPlayer!=null) {
+        	    		  Log.d(TAG, "Runnable called");
+            	    	  registerForContextMenu(v);
+            	    	  openContextMenu(v);
+        	    	  }  
+        	      }	  
+        	    }, 5000);
         	}
+        		
          });
     }
     
-    private void playVideo() {
- 	   	String url=editTextSongURL.getText().toString();
+     void playVideo(String url) {
+ 	   	
 
     	try {
     		videoPlayer = new MediaPlayer();
@@ -71,9 +98,13 @@ public class PlayingVideo extends Activity implements OnCompletionListener, OnPr
     		videoPlayer.prepare();
     		videoPlayer.setOnPreparedListener(this);
     		videoPlayer.setOnCompletionListener(this);
+    		metaRetriever.setDataSource(url);
+    		bRate = Integer.parseInt(metaRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_BITRATE))/1000.0;
     		mController = new MediaController(this);
     	
     	} catch (IOException e) {
+    		Toast.makeText(this, "Could not open file " + url + " for playback.",
+                    Toast.LENGTH_SHORT).show();
     		Log.e(TAG, "Could not open file " + url + " for playback.", e);
     	}
     }
@@ -84,18 +115,8 @@ public class PlayingVideo extends Activity implements OnCompletionListener, OnPr
 		mController.setMediaPlayer(this);
 	    mController.setAnchorView(findViewById(R.id.surfacePlayer));
 	    videoPlayer.start();
-	    //Do something after 5000ms
-	    final Handler handler = new Handler();
-	    handler.postDelayed(new Runnable() {
-	      @Override
-	      public void run() {
-	        
-	      }
-	    }, 5000);
+	}    
 	    
-	    myIntent = new Intent(PlayingVideo.this, MyAlarmService.class);
-        startService(myIntent);
-	}
 	
 	/** This callback will be invoked when the file is finished playing */
 	@Override
@@ -108,22 +129,34 @@ public class PlayingVideo extends Activity implements OnCompletionListener, OnPr
         startActivity(myintent);
 	}
 	
+		@Override
+    protected void onPause() {
+        super.onPause();
+        if (videoPlayer != null && videoPlayer.isPlaying()) {
+        	mController.hide();
+        	videoPlayer.pause();
+        }
+    }
+	
+	@Override
+	protected void onResume() {
+	    super.onResume();
+	    if (videoPlayer != null && videoPlayer.isPlaying()) {
+        	mController.show();
+        	videoPlayer.start();
+        }
+	}
+
 	@Override
 	protected void onStop() {
 	    super.onStop();
 	    releaseMediaPlayer();
 	}
-	
+		
 	@Override
-    protected void onPause() {
-        super.onPause();
-        releaseMediaPlayer();
-    }
-
-    @Override
     protected void onDestroy() {
-        super.onDestroy();
         releaseMediaPlayer();
+        super.onDestroy();
     }
 
     private void releaseMediaPlayer() {
@@ -132,20 +165,20 @@ public class PlayingVideo extends Activity implements OnCompletionListener, OnPr
      	    videoPlayer.stop();
             videoPlayer.release();
             videoPlayer = null;
-            stopService(myIntent);
         }
     }
 	
 	/**  Use screen touches to toggle the video between playing and paused. */
 	@Override
 	public boolean onTouchEvent (MotionEvent ev){	
-		mController.show();
-		if(ev.getAction() == MotionEvent.ACTION_DOWN){
+		
+		if(videoPlayer != null && ev.getAction() == MotionEvent.ACTION_DOWN){
 			if(videoPlayer.isPlaying()){
 				videoPlayer.pause();
 			} else {
 				videoPlayer.start();
 			}
+			mController.show();
 			return true;		
 		} else {
 			return false;
@@ -170,8 +203,8 @@ public class PlayingVideo extends Activity implements OnCompletionListener, OnPr
 		
 	}
 	
-	//mediacontroller implemented methods
 
+	//MediaController implemented methods
     public void start() {
         videoPlayer.start();
     }
@@ -210,6 +243,51 @@ public class PlayingVideo extends Activity implements OnCompletionListener, OnPr
 
     public boolean canSeekForward() {
         return true;
+    }
+    
+    
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+       // AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        //MenuInflater inflater = getMenuInflater();
+        //inflater.inflate(R.menu.options, menu);
+        try {
+			 curBrightness = android.provider.Settings.System.getInt(getContentResolver(), android.provider.Settings.System.SCREEN_BRIGHTNESS);
+		} catch (SettingNotFoundException e) {
+			e.printStackTrace();
+		  }
+		 
+        menu.setHeaderTitle("Switch context");
+        bLevel = BatteryCalc();
+        rTime = BATTERY_CONST - (curBrightness * BRIGHT_CONST + bRate * BITRATE_CONST);
+        rTime = rTime * bLevel; 
+        //Log.d(TAG, "Battery level is at " + bLevel );
+        
+        menu.add(0, ID_MENU_PRESENT, 0 , "Present Config(" + 
+        		(int)Math.round(rTime) / 60 + "hr " +
+				(int)Math.round(rTime) % 60 +" min)");
+        
+        menu.add(0, R.id.brightness, 0, "Brightness"+ "_time");
+
+        menu.add(0, R.id.bitrate, 0, "Bitrate"+ "_time");
+    }
+    
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+       // AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+     
+        switch (item.getItemId()) {
+        case R.id.brightness:
+        	 Toast.makeText(this, "You have chosen the " + item.getTitle().toString() + " menu option",
+                     Toast.LENGTH_SHORT).show();
+        	 return true;
+        case R.id.bitrate:
+       	 	 Toast.makeText(this, "You have chosen the " + item.getTitle().toString() + " menu option",
+                    Toast.LENGTH_SHORT).show();
+       	 	 return true;
+        }
+        return false;
     }
 		
 }
